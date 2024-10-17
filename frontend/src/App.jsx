713a54a7bbc0cd79c28d59333e93408e042f6d2f
Usman Shahid <usmanshahid027@outlook.com>
2024-10-17 22:36:57 +0500

    completed basic layout for signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 // import './index.css'
 import Login from './pages/login/Login.jsx';
+import Signup from './pages/signup/Signup.jsx';
 const App = () => {
 
   return (
     <>
     <h1>Testing </h1>
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Signup />
     </div>
     </>
   );
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
+    <>
+    <div className='flex flex-col items-center justify-center min-w-36 mx-auto'>
+      <div className="w-full p-6 rounded-lg shadow-d bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <p className="text-4xl text-center text-white"><strong>Signup <span className="text-blue-200">MERN Chat App</span></strong></p>
+
+        <form action="">
+          <label htmlFor="" className="label p-2"><span className="text-white label-text">Full Name</span></label>
+          <input type="text" className="w-full input input-bordered h-10" placeholder='Enter your Full Name' name='name'/>
+
+          <label htmlFor="" className="label p-2"><span className="text-white label-text">Username</span></label>
+          <input type="text" className="w-full input input-bordered h-10" placeholder='Enter your Unique Username' name='username' />
+
+          <label htmlFor="" className="label p-2"><span className="text-white label-text">Gender</span></label>
+          <select type="text" className="w-full input input-bordered h-10" placeholder='Enter your Full Name' defaultValue={''} name='gender'>
+            <option value="" disabled>Select Your Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+
+          <label htmlFor="" className="label p-2"><span className="text-white label-text">Password</span></label>
+          <input type="password" className="w-full input input-bordered h-10" placeholder='Enter your password' name='password' />
+
+          <label htmlFor="" className="label p-2"><span className="text-white label-text">Confirm Password</span></label>
+          <input type="password" className="w-full input input-bordered h-10" placeholder='Confirm your password' name='confirm_password' />
+
+          <div className="text-center">
+            <button className="btn btn-block btn-sm mt-2" type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
       
     </div>
-  )
+    </>
+  );
 }
 
 export default Signup
