9648f20a036f46e45712f5cab57df922c149ed87
Usman Shahid <usmanshahid027@outlook.com>
2024-10-18 20:08:17 +0500

    completed working on login hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import toast from 'react-hot-toast';
+import {useAuthContext} from '../context/AuthContext';
+
+const useLogin = () => {
+    const [loading, setLoading] = useState(false);
+    const {setAuthUser} = useAuthContext();
+
+    const login = async (username, password) => {
+        try {
+            setLoading(true);
+            const res = await fetch(
+                `${import.meta.env.VITE_API_HOST}/api/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({username, password})
+                }
+            );
+
+            const data = await res.json();
+            console.log(data);
+
+            if (data.error)
+                throw new Error(data.error);
+
+            localStorage.setItem('chat-user', JSON.stringify(data));
+            setAuthUser(data);
+
+            toast.success('You Are Now Logged Into The Website');
+        } catch(error) {
+            toast.error(error.message || error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return {loading, login};
+}
+
+export default useLogin;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {loading, login} = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-36 mx-auto'>
       <div className="w-full p-6 rounded-lg shadow-d bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <p className="text-4xl text-center text-white"><strong>Login <span className="text-blue-200">MERN Chat App</span></strong></p>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="" className="label p-2"><span className="text-white label-text">Username</span></label>
-          <input type="text" className="w-full input input-bordered h-10" placeholder='Enter your username' />
+          <input type="text" className="w-full input input-bordered h-10" placeholder='Enter your username' 
+          required
+            minLength={5}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
           <label htmlFor="" className="label p-2"><span className="text-white label-text">Password</span></label>
-          <input type="password" className="w-full input input-bordered h-10" placeholder='Enter your password' />
+          <input type="password" className="w-full input input-bordered h-10" placeholder='Enter your password' 
+            required
+            minLength={8}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
           <div className="text-center">
-            <button className="btn btn-block btn-sm mt-2" type='submit'>Submit</button>
+            <button className="btn btn-block btn-sm mt-2" type='submit' disabled={loading}>{!loading?'Submit': (<span className='loading loading-spinner text-white'></span>)}</button>
           </div>
           <div className="text-center mt-2">
           <span className="text-lg text-white">Create an account, <Link to='/signup' className='text-blue-300'>Signup</Link></span>
