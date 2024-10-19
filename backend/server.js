efdbf315a44736a4f0394ccff45a01f8eac4a332
Usman Shahid <usmanshahid027@outlook.com>
2024-10-19 22:58:03 +0500

    again a try for fixing these issues!

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: 'https://mernchat.app.genez.io',
-    credentials: true
+    // credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
