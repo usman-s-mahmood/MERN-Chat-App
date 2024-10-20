3033952740e865ce92ff5351f889e2735aff021f
Usman Shahid <usmanshahid027@outlook.com>
2024-10-20 14:21:13 +0500

    testing API status

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// app.get(
-//     '/',
-//     (req, res) => {
-//         res.send("<h1>Server Is Running</h1>");
-//     }
-// );
+app.get(
+    '/',
+    (req, res) => {
+        res.send("<h1>Server Is Running</h1>");
+    }
+);
 
 app.use(
     '/api/auth',
