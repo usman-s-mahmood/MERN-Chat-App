0d67ae9b90d0dd63bdcb4f551e8b554acf354a97
Usman Shahid <usmanshahid027@outlook.com>
2024-10-20 14:20:35 +0500

    again fixing deployment issues

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+// });
 
 server.listen(
     PORT,
