9f3564379e1d2921603830b87f50ca786c99425f
Usman Shahid <usmanshahid027@outlook.com>
2024-10-24 21:52:18 +0500

    again commiting to fix these errors

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URI || 'mongodb+srv://ojafivofi:fyytfjyb7ry6aRop@cluster0.09zwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log(`Connected To Mongo DB`);
     } catch(error) {
         console.error(`Error Connecting To MongoDB: ${error}`);
